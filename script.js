736c01d67c0197a7376e93cb551d7b50b09289e9
David Stewart <darkorin00@hotmail.com>
2020-05-25 04:20:29 -0400

    finished working for the night.

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ let modeBtn = $("#modeSwitch");
 let btnMode = "Dark";
 
 const changeMode = () => {
+    
     document.body.style.backgroundImage = `url("assets/${btnMode}mode.png")`;
     $("#navMain").attr("class", `navbar navbar-expand-lg transparent navbar-${btnMode.toLowerCase()}`);
+    
+
+    let btnList = $(".switchImg");
+    $.each(btnList, i => {
+        $(btnList[i]).attr("src", `assets/${btnMode}${btnList[i].id}.png`);
+    });
+
     switch (btnMode) {
         case "Dark":
             btnMode = "Light";
@@ -16,9 +24,8 @@ const changeMode = () => {
     }
     modeBtn.attr("class", `btn btn-${btnMode.toLowerCase()}`);
     modeBtn.text(`${btnMode} Mode`);
-    let swapText = $(".switchModes");
-    console.log(swapText);
-    $.each(swapText, (i) => {
+
+    const textSwapper = i => {
         let currentEle = $(swapText[i]);
         if (currentEle.hasClass("text-dark")) {
             currentEle.removeClass("text-dark");
@@ -27,7 +34,10 @@ const changeMode = () => {
             currentEle.removeClass("text-light");
             currentEle.addClass("text-dark");
         }
-    });
+    }
+
+    let swapText = $(".switchModes");
+    $.each(swapText, textSwapper);
 }
 
 modeBtn.on("click", changeMode);

09d5d23d46a2cfe09b9d826d7d21716719d0dd92
David Stewart <darkorin00@hotmail.com>
2020-09-07 02:10:51 -0400

    Updating React Build

---
+++ build/precache-manifest.613249eab2938cc8e173fee26f2540bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0206c09aea6285caf8adb8ce1d82fec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32072c3e5757dc67b690",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbce5465f2bdf483c021",
+    "url": "/static/js/2.48e6354f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.48e6354f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32072c3e5757dc67b690",
+    "url": "/static/js/main.eb02045b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);

bee35861630ddd458fe48bc53f3cc607bafdadf6
David Stewart <darkorin00@hotmail.com>
2021-03-09 04:05:24 -0500

    last deployment fix

---
+++ precache-manifest.f8a869652bd017143feacf3847c62f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95e90ecd08fcbf697c1d4ba2d93b354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b27fde84d39becc24d1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbce5465f2bdf483c021",
+    "url": "/static/js/2.48e6354f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.48e6354f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b27fde84d39becc24d1",
+    "url": "/static/js/main.a3b9f4d8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);

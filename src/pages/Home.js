919bbe9bae1336844628a3f9ec9335e01a9aceed
David Stewart <darkorin00@hotmail.com>
2021-03-09 03:14:28 -0500

    updating portfolio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ function Home(props) {
       <br /><br /><br /><br />
       <img className="portrait" src={props.images.portrait} alt="Portrait of David" />
       <br /><br /><br />
-      <h1 className={`switchModes text-${props.theme.foreground.toLowerCase()}`} id="intro">Hi, my name is David and I develop web pages!</h1>
+      <h1 className={`switchModes text-${props.theme.foreground.toLowerCase()}`} id="intro">Hi, my name is David Stewart, Co-Founder and Lead Engineer at Common.gg</h1>
       <br /><br />
       <Icon theme={props.theme} type="Github" link="https://github.com/Darkorin" text="" />
       <Icon theme={props.theme} type="Linkedin" link="https://www.linkedin.com/in/david-s-stewart/" text="" />
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Project from "../components/Project.js";
 
 function Projects(props) {
   return (
-    <div>Fix projects</div>
-  ) 
+    <div className="container row">
+      <div className="col-2"></div>
+      <div className="col-8 row">
+        {props.projects.map(project => (
+          <div className="col-12">
+            <Project project={project} theme={props.theme} /><br />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Projects;

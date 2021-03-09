8c900e388cde8e2020d6cdc8f8ffe918a4a7b32e
David Stewart <darkorin00@hotmail.com>
2021-03-09 03:54:54 -0500

    fixing deployment

---
+++ precache-manifest.8df05b635fde03bf24daef39a77fe8b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19de3ed1a94051a7764ccb1b047c3fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7712da9caf713d8163f7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbce5465f2bdf483c021",
+    "url": "/static/js/2.48e6354f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.48e6354f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7712da9caf713d8163f7",
+    "url": "/static/js/main.5aa9fe2b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);

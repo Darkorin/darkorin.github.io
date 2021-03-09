730e6b1a142441ace18004cd04e75d430f1597d5
David Stewart <darkorin00@hotmail.com>
2021-03-09 03:29:26 -0500

    updating portfolio

---
+++ precache-manifest.29871620a37baed97be8eb8f99c7f0ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f05eff220c2946bce74acc7de17195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1aefe53ad2f37338d22",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbce5465f2bdf483c021",
+    "url": "/static/js/2.48e6354f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.48e6354f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1aefe53ad2f37338d22",
+    "url": "/static/js/main.2f432637.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
